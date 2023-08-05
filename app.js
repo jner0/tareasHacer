8c83a0143fddac7683ed5e3b297be8cb30eebf3b
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-08-04 23:18:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require("colors");
+const { inquirerMenu, pausa } = require("./helpers/inquirer");
+
+console.clear();
+
+const main = async () => {
+  console.log("Hola Mundo");
+
+  let opt = "";
+
+  do {
+    opt = await inquirerMenu();
+    console.log({ opt });
+
+    await pausa();
+  } while (opt !== "0");
+};
+
+main();
